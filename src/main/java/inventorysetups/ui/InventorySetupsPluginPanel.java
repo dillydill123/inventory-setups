1b2773ff3da4f87bf43c1ddfed34ceb7f8534944
Dillon <>
2022-07-02 17:30:37 -0400

    Checkstyle fixes

--- src/main/java/inventorysetups/ui/InventorySetupsPluginPanel.java
+++ src/main/java/inventorysetups/ui/InventorySetupsPluginPanel.java
@@ -38,7 +38,6 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import lombok.Getter;
 import net.runelite.api.InventoryID;
